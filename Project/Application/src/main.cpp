1d012dd58b46dc825825c66d90bf388e05030a55
Olawole <ayolawole@gmail.com>
2024-11-25 23:29:30 +0000

    Project: grid setup

--- Project/Application/src/main.cpp
+++ Project/Application/src/main.cpp
@@ -1,28 +1,6 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
+//OLAWOLE ABAYOMI-OWODUNNI
+//C00278711
 
-For a C++ project simply rename the file to .cpp and re-run the build script 
-
--- Copyright (c) 2020-2024 Jeffery Myers
---
---This software is provided "as-is", without any express or implied warranty. In no event 
---will the authors be held liable for any damages arising from the use of this software.
-
---Permission is granted to anyone to use this software for any purpose, including commercial 
---applications, and to alter it and redistribute it freely, subject to the following restrictions:
-
---  1. The origin of this software must not be misrepresented; you must not claim that you 
---  wrote the original software. If you use this software in a product, an acknowledgment 
---  in the product documentation would be appreciated but is not required.
---
---  2. Altered source versions must be plainly marked as such, and must not be misrepresented
---  as being the original software.
---
---  3. This notice may not be removed or altered from any source distribution.
-
-*/
 #include "stdio.h"
 #include <random>
 
@@ -33,43 +11,53 @@ For a C++ project simply rename the file to .cpp and re-run the build script
 
 #include <cmath>
 
-int main ()
-{
+void application();
+void drawing();
 
-
-	// Tell the window to use vysnc and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-
-	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
-
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
+	//VARIABLES
+	static const int SCREEN_WIDTH = 1280;
+	static const int SCREEN_HEIGHT = 800;
+	const int CANVAS_WIDTH = 600;
+	const int CANVAS_HEIGHT = 600;
+	const int CELL_SIZE = 25;
+	const int TOTAL_COLS = CANVAS_WIDTH / CELL_SIZE;
+	const int TOTAL_ROWS = CANVAS_HEIGHT / CELL_SIZE;
+	int transGrid[TOTAL_ROWS][TOTAL_COLS] = {};
+	int pixelGrid[TOTAL_ROWS][TOTAL_COLS] = {};
 
 	// Load a texture from the resources directory
 	Texture wabbit = LoadTexture("wabbit_alpha.png");
 
-	//VARIABLES
+int main ()
+{
+	// Tell the window to use vysnc and work on high DPI displays
+	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+
+	// Create the window and OpenGL context
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Olawole's Pixel Editor");
+
+	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+	SearchAndSetResourceDir("resources");
+
+	for (int row = 0; row < TOTAL_ROWS; row++) {
+		for (int col = 0; col < TOTAL_COLS; col++) {
+			transGrid[row][col] = 0;
+			pixelGrid[row][col] = 0;
+			
+			int x = col * CELL_SIZE;
+			int y = row * CELL_SIZE;
+
+			if (row % 2 == 0) DrawRectangle(x, y, CELL_SIZE, CELL_SIZE, GRAY);
+			else DrawRectangle(x, y, CELL_SIZE, CELL_SIZE, WHITE);
+		}
+	}
 
 
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
-
-		// drawing
-		BeginDrawing();
-
-		// Setup the backbuffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
-
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200, 200, 20, WHITE);
-
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
-
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
-		EndDrawing();
+		application();
+;		drawing();
 	}
 
 	// cleanup
@@ -79,4 +67,42 @@ int main ()
 	// destory the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
+}
+
+void application() {
+	// drawing
+	BeginDrawing();
+
+	// Setup the backbuffer for drawing (clear color and depth buffers)
+	ClearBackground(BLACK);
+
+	for (int row = 0; row < TOTAL_ROWS; row++) {
+		for (int col = 0; col < TOTAL_COLS; col++) {
+
+			int x = 350 + (col * CELL_SIZE);
+			int y = 100 + (row * CELL_SIZE);
+
+			if (row % 2 == 0) {
+				if (col % 2 == 0) DrawRectangle(x, y, CELL_SIZE, CELL_SIZE, GRAY);
+				else DrawRectangle(x, y, CELL_SIZE, CELL_SIZE, BLACK);
+			}
+			else {
+				if (col % 2 == 1) DrawRectangle(x, y, CELL_SIZE, CELL_SIZE, GRAY);
+				else DrawRectangle(x, y, CELL_SIZE, CELL_SIZE, BLACK);
+			}
+		}
+	}
+}
+
+void drawing() {
+
+	// draw some text using the default font
+	DrawText("Pixel Editor", 550, 50, 30, WHITE);
+
+	// draw our texture to the screen
+	DrawTexture(wabbit, 400, 200, BLACK);
+
+	// end the frame and get ready for the next one  (display frame, poll input, etc...)
+	EndDrawing();
+
 }
